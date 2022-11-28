3e34dafe20e005f719b3ab0a25612ad6f64b4568
Joao Marques <joaomarques2176@gmail.com>
2022-11-27 21:00:32 -0300

    Criacao pasta Empresa

---
+++ Web/empregamais-website/src/Empresa/Empresa.js
@@ -0,0 +1,10 @@
+import { Header } from "../Shared/Header"
+
+export const Empresa = () => {
+    return (
+        <div>
+            <Header headerHome='true'/>
+            <h1>Empresa</h1>
+        </div>
+    )
+}
